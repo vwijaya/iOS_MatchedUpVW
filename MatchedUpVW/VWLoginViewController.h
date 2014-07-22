0c9b2660b618d3bc635cffb5f1933a388753dab1
Valerino Wijaya <valerino@gmail.com>
2014-07-22 09:35:39 +0700

    MatchedUpVW: Lec 291 Setting up Xcode

---
+++ MatchedUpVW/VWLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  VWLoginViewController.h
+//  MatchedUpVW
+//
+//  Created by Valerino on 7/22/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWLoginViewController : UIViewController
+
+@end
---
+++ MatchedUpVW/VWLoginViewController.m
@@ -0,0 +1,57 @@
+//
+//  VWLoginViewController.m
+//  MatchedUpVW
+//
+//  Created by Valerino on 7/22/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import "VWLoginViewController.h"
+
+@interface VWLoginViewController ()
+
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
+@end
+
+@implementation VWLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - IBActions
+
+- (IBAction)loginButtonPressed:(UIButton *)sender {
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
